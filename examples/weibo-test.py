21f4f78327aa099046820cefbf3a55b5a6d6beb8
zhangyangyu <zhangyangyu0614@gmail.com>
2013-05-11 09:22:30 +0800

    add weibo module

--- examples/weibo-test.py
+++ examples/weibo-test.py
@@ -1,30 +1,30 @@
 import tornado.ioloop
 import tornado.web
-import tornadocnauth.Baidu
+import tornadocnauth.Weibo
 
 from tornado import gen
 
-class AuthHandler(tornado.web.RequestHandler, tornadocnauth.Baidu.BaiduMixin):
+class AuthHandler(tornado.web.RequestHandler, tornadocnauth.Weibo.WeiboMixin):
     @tornado.web.asynchronous
     @gen.coroutine
     def get(self):
         if self.get_argument('code', None):
             user = yield self.get_authenticated_user(
                 redirect_uri='YOUR_REDIRECT_URI',
-                client_id=self.settings['baidu_api_key'],
-                client_secret=self.settings['baidu_api_secret'],
+                client_id=self.settings['weibo_api_key'],
+                client_secret=self.settings['weibo_api_secret'],
                 code=self.get_argument('code'))
-            self.render('main.html', user=user)
+            self.render('weibo.html', user=user)
         else:
             self.authorize_redirect(
-                redirect_uri="YOUR_REDIRECT_URI",
-                client_id=self.settings['baidu_api_key']
-               )
+                redirect_uri='YOUR_REDIRECT_URI',
+                client_id=self.settings['weibo_api_key']
+                )
 
 app = tornado.web.Application([
         ('/', AuthHandler),
-        ], baidu_api_key='YOUR_API_KEY',
-                              baidu_api_secret='YOUR_API_SECRET', debug=True)
+        ], weibo_api_key='YOUR_API_KEY',
+                              weibo_api_secret='YOUR_API_SECRET')
 
 if __name__ == '__main__':
     app.listen(80)
